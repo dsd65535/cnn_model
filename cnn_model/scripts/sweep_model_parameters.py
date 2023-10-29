405a4a9924926df89c5e4b643efa9ee791ec83bd
Dimitar Dimitrov <dimitrov.d@northeastern.edu>
2023-10-29 17:29:52 -0400

    use test_and_train in sweep_model_parameters

--- cnn_model/scripts/sweep_model_parameters.py
+++ cnn_model/scripts/sweep_model_parameters.py
@@ -1,54 +1,35 @@
 # pylint:disable=duplicate-code
 """This script sweeps the model parameters"""
-import torch
-
-from cnn_model.basic import get_device
+from cnn_model.__main__ import train_and_test
 from cnn_model.basic import test_model
-from cnn_model.basic import train_model
-from cnn_model.datasets import get_dataset_and_params
-from cnn_model.models import Main
 
 
 def run(
-    train_dataloader: torch.utils.data.DataLoader,
-    test_dataloader: torch.utils.data.DataLoader,
     *,
-    device: str,
+    dataset_name: str,
+    batch_size: int,
     lr: float,
     count_epoch: int,
-    in_size: int,
-    in_channels: int,
     conv_out_channels: int,
     kernel_size: int,
     stride: int,
     padding: int,
     pool_size: int,
-    feature_count: int,
 ) -> float:
-    # pylint:disable=too-many-arguments,too-many-locals
+    # pylint:disable=too-many-arguments
     """Run a test"""
 
-    model = Main(
-        in_size=in_size,
-        in_channels=in_channels,
+    model, loss_fn, test_dataloader, device = train_and_test(
+        lr=lr,
+        count_epoch=count_epoch,
+        dataset_name=dataset_name,
+        batch_size=batch_size,
         conv_out_channels=conv_out_channels,
         kernel_size=kernel_size,
         stride=stride,
         padding=padding,
         pool_size=pool_size,
-        feature_count=feature_count,
-    ).to(device)
-    loss_fn = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
-
-    for _ in range(count_epoch):
-        train_model(
-            model,
-            train_dataloader,
-            loss_fn,
-            optimizer,
-            device=device,
-        )
+    )
 
     _, accuracy = test_model(model, test_dataloader, loss_fn, device=device)
 
@@ -67,69 +48,49 @@ def main(
     padding: int = 0,
     pool_size_def: int = 2,
 ) -> None:
-    # pylint:disable=too-many-arguments,too-many-locals
+    # pylint:disable=too-many-arguments
     """Main Function"""
-
-    device = get_device()
-
-    (train_dataloader, test_dataloader), (
-        in_channels,
-        in_size,
-        feature_count,
-    ) = get_dataset_and_params(name=dataset_name, batch_size=batch_size)
 
     for conv_out_channels_exp in range(8):
         conv_out_channels = 2**conv_out_channels_exp
         accuracy = run(
-            train_dataloader,
-            test_dataloader,
-            device=device,
+            dataset_name=dataset_name,
+            batch_size=batch_size,
             lr=lr,
             count_epoch=count_epoch,
-            in_size=in_size,
-            in_channels=in_channels,
             conv_out_channels=conv_out_channels,
             kernel_size=kernel_size_def,
             stride=stride,
             padding=padding,
             pool_size=pool_size_def,
-            feature_count=feature_count,
         )
         print(f"conv_out_channels = {conv_out_channels}: {accuracy*100}%")
 
     for kernel_size in range(1, 8):
         accuracy = run(
-            train_dataloader,
-            test_dataloader,
-            device=device,
+            dataset_name=dataset_name,
+            batch_size=batch_size,
             lr=lr,
             count_epoch=count_epoch,
-            in_size=in_size,
-            in_channels=in_channels,
             conv_out_channels=conv_out_channels_def,
             kernel_size=kernel_size,
             stride=stride,
             padding=padding,
             pool_size=pool_size_def,
-            feature_count=feature_count,
         )
         print(f"kernel_size = {kernel_size}: {accuracy*100}%")
 
     for pool_size in range(1, 5):
         accuracy = run(
-            train_dataloader,
-            test_dataloader,
-            device=device,
+            dataset_name=dataset_name,
+            batch_size=batch_size,
             lr=lr,
             count_epoch=count_epoch,
-            in_size=in_size,
-            in_channels=in_channels,
             conv_out_channels=conv_out_channels_def,
             kernel_size=kernel_size_def,
             stride=stride,
             padding=padding,
             pool_size=pool_size,
-            feature_count=feature_count,
         )
         print(f"pool_size = {pool_size}: {accuracy*100}%")
 
